39144b95ec2f2d9c187929701fcbfad405c8a651
Anderson Dias <andersoninfonet@gmial.com>
2023-06-19 03:05:29 -0300

    refatoração app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, AfterViewInit, ChangeDetectorRef, Component, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { AutenticacaoService } from './services/autenticacao.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  isCollapsed = false;
+export class AppComponent implements OnInit {
+
+  isLoged!: boolean;
+  sub!: Subscription;
+
+  constructor(
+    private autenticacaoService: AutenticacaoService,
+    private cd: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.sub = this.autenticacaoService.mostraBotaoLogout.subscribe(value => {
+      this.isLoged = value
+    });
+  }
+
 }
