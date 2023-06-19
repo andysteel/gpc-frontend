5373127e83281d0537768fce7474b04e7fa1ba96
Anderson Dias <andersoninfonet@gmial.com>
2023-06-19 03:52:19 -0300

    teste deploy

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   envVar: {
-      GCLIENT_ID: "",
-      BASE_GPC_URL: ""
+      GCLIENT_ID: "294064042774-8qk8hn421ugqcss9o8kkvgrucp0la345.apps.googleusercontent.com",
+      BASE_GPC_URL: "https://gpc-app.andersondias.net.br/api"
 }
 };
